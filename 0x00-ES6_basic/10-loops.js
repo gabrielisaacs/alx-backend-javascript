06ee8188230bbd9fc9688527338de5d7a25d5fe5
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-01 03:49:30 +0100

    re: for...of loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
+  let index = 0;
   for (const value of array) {
-    const index = array.indexOf(value);
-    array[index] = appendString + value;
+    array[index++] = appendString + value;
   }
 
   return array;
