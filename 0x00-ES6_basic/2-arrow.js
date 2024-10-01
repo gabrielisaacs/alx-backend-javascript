322fd71c4e19053d5118a74fcdd1dc5f88f5f27c
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-01 04:09:27 +0100

    add comments

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,8 +1,9 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
+  // Use 'this' directly in an arrow function
   this.addNeighborhood = (newNeighborhood) => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
+    this.sanFranciscoNeighborhoods.push(newNeighborhood); // Add new neighborhood
+    return this.sanFranciscoNeighborhoods; // Return updated list
   };
 }
