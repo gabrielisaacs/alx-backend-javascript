61091bfbd1e65232c31700f09ab5ff97452f3dfe
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-01 04:03:09 +0100

    add comments

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
+  // Create and return an object with the department name as the key
+  // and the employees array as its value.
   return {
-    [departmentName]: employees,
+    [departmentName]: employees, // Use computed property names to set the department name as the key
   };
 }
