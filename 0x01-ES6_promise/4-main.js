3ffa6903973a044033628b806d641ee87acb23b5
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-04 12:07:07 +0100

    implement ClassRoom class

--- 0x01-ES6_promise/4-main.js
+++ 0x01-ES6_promise/4-main.js
@@ -1,3 +1,4 @@
+// entry point
 import signUpUser from "./4-user-promise";
 
 console.log(signUpUser("Bob", "Dylan"));
---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+// Implement ClassRoom class
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
