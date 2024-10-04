15b7a504cd932cee69d86a89a05b73ae58bef678
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-04 12:39:34 +0100

    fix linter issues

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,15 @@
-import ClassRoom from "./0-classroom";
+// Import the ClassRoom class from 0-classroom.js
+import ClassRoom from "./0-classroom.js";
 
+/**
+ * Initializes and returns an array of ClassRoom objects.
+ * @returns {Array} An array containing three ClassRoom objects
+ * with max student sizes of 19, 20, and 34.
+ */
 export default function initializeRooms() {
   return [
-    new ClassRoom(19), // Create ClassRoom with 19 students
-    new ClassRoom(20), // Create ClassRoom with 20 students
-    new ClassRoom(34), // Create ClassRoom with 34 students
+    new ClassRoom(19), // ClassRoom with 19 students
+    new ClassRoom(20), // ClassRoom with 20 students
+    new ClassRoom(34), // ClassRoom with 34 students
   ];
 }
