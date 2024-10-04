813aec37d950694373c13365f09f17332811bef1
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-04 12:47:05 +0100

    fix lint

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,6 @@
 // Import the ClassRoom class from 0-classroom.js
 import ClassRoom from "./0-classroom.js";
 
-/**
- * Initializes and returns an array of ClassRoom objects.
- * @returns {Array} An array containing three ClassRoom objects
- * with max student sizes of 19, 20, and 34.
- */
 export default function initializeRooms() {
   return [
     new ClassRoom(19), // ClassRoom with 19 students
