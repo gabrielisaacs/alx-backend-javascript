1a25fc49ce4cc97e0305059395f64a6a8b4aed02
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-04 04:19:55 +0100

    promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,14 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulating an async API call
+    setTimeout(() => {
+      // resolve with a dummy response
+      resolve({
+        status: 200,
+        body: 'photo-profile-1',
+      });
+    }, 1000); // Simulate a delay of 1 second
+  });
+}
+
+export default getResponseFromAPI;
