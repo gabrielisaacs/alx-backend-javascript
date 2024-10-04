dc9f82e4695dea3b184dc3933b97ed3038fe9244
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-04 14:45:16 +0100

    remove quotes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -14,8 +14,8 @@ export default class HolbertonCourse {
 
   // Setter for name with type validation
   set name(value) {
-    if (typeof value !== "string") {
-      throw new TypeError("Name must be a string");
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
     }
     this._name = value;
   }
@@ -27,8 +27,8 @@ export default class HolbertonCourse {
 
   // Setter for length with type validation
   set length(value) {
-    if (typeof value !== "number") {
-      throw new TypeError("Length must be a number");
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
     }
     this._length = value;
   }
@@ -42,9 +42,9 @@ export default class HolbertonCourse {
   set students(value) {
     if (
       !Array.isArray(value) ||
-      !value.every((student) => typeof student === "string")
+      !value.every((student) => typeof student === 'string')
     ) {
-      throw new TypeError("Students must be an array of strings");
+      throw new TypeError('Students must be an array of strings');
     }
     this._students = value;
   }
--- 0x02-ES6_classes/2-main.js
+++ 0x02-ES6_classes/2-main.js
@@ -2,19 +2,19 @@ import HolbertonCourse from "./2-hbtn_course.js";
 
 // Create a new course object
 const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"]);
-console.log(c1.name); // Output: ES6
+console.log(c1.name);
 c1.name = "Python 101"; // Changing the name attribute
 console.log(c1); // Output the updated object
 
 // Test for type validation
 try {
-  c1.name = 12; // This should throw an error
+  c1.name = 12;
 } catch (err) {
-  console.log(err); // Output: TypeError: Name must be a string
+  console.log(err);
 }
 
 try {
-  const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]); // This should throw an error
+  const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
 } catch (err) {
-  console.log(err); // Output: TypeError: Length must be a number
+  console.log(err);
 }
