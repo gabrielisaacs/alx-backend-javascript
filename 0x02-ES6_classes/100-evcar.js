697ba9c65789c2733ba3bd09985d3cc810d93164
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-04 19:14:31 +0100

    update evcar class

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,33 +1,13 @@
-// Import Car class
 import Car from './10-car.js';
 
-/**
- * Represents an Electric Vehicle Car (EVCar).
- *
- * @class
- * @extends Car
- */
+// EVcar class
 export default class EVCar extends Car {
-  /**
-   * Create a new EVCar instance.
-   *
-   * @param {string} brand - The brand of the car.
-   * @param {string} motor - The type of motor in the car.
-   * @param {string} color - The color of the car.
-   * @param {string} range - The driving range of the car.
-   */
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  /**
-   * Clone the EVCar object but return a Car object for privacy.
-   *
-   * @returns {Car} A new instance of the Car class.
-   */
   cloneCar() {
-    // Return a new instance of Car instead of EVCar
     return new Car(this._brand, this._motor, this._color);
   }
 }
