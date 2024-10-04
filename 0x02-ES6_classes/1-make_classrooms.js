5b4627950005e6a5046876326369e422215005f5
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-04 15:19:39 +0100

    remove comments

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,6 @@
 // Import the ClassRoom class from 0-classroom.js
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  return [
-    new ClassRoom(19), // ClassRoom with 19 students
-    new ClassRoom(20), // ClassRoom with 20 students
-    new ClassRoom(34), // ClassRoom with 34 students
-  ];
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
