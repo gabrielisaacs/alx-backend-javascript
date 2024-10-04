1d8f856087f80fe64f49495b216a5287d74d8f4b
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-04 05:15:23 +0100

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-main.js
@@ -0,0 +1,4 @@
+// Test the handleProfileSignup(firstName, lastName, fileName) function
+import handleProfileSignup from './6-final-user';
+
+console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
