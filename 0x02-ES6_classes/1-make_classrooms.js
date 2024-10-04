9ff6299a66429e326a91199b108334f99744e313
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-04 15:41:40 +0100

    remove trailing space

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,5 @@
 // Import the ClassRoom class from 0-classroom.js
 import ClassRoom from './0-classroom.js';
-
 export default function initializeRooms() {
   return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
