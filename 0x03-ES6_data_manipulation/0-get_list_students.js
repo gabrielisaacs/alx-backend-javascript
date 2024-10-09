274b046aa81ce491169fc80615b9d978ebe2d26c
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-09 11:26:18 +0100

    new fix

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,4 @@
-export default function getListStudents() {
+function getListStudents() {
   const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
@@ -6,3 +6,5 @@ export default function getListStudents() {
   ];
   return students;
 }
+
+export default getListStudents;
