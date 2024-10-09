aa22aefaea592c74f604896e79790b9870681af4
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-09 06:49:18 +0100

    export function

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,7 +2,7 @@
  * Returns an array of student objects with id, firstName, and location attributes.
  * @returns {Array<Object>} Array of student objects.
  */
-function get_list_students(){
+export default function get_list_students(){
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
