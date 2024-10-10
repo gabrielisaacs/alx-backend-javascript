e20fa2a7cc2dbb47f50cb28e9f382f897826ca9f
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-10 05:05:35 +0100

    int8 fix

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,14 +2,18 @@ function createInt8TypedArray(length, position, value) {
   // Create a new ArrayBuffer of the specified length
   const buffer = new ArrayBuffer(length);
 
+  // Create a DataView to manipulate the buffer
   const dataView = new DataView(buffer);
 
+  // Check if the position is within the valid range
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
 
+  // Set the Int8 value at the specified position
   dataView.setInt8(position, value);
 
+  // Return the DataView
   return dataView;
 }
 
