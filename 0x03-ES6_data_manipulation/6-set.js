2ffc599fab868558915b236b6f54648592e45aba
Gabriel Isaac <stewarti3@yahoo.com>
2024-10-10 05:34:02 +0100

    array set

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,7 +1,6 @@
-export default function setFromArray(arrayset) {
-  if (arrayset instanceof Array) {
-    return new Set(arrayset);
-    }
+export default function setFromArray(array) {
+  if (array instanceof Array) {
+    return new Set(array);
+  }
   return [];
 }
-
