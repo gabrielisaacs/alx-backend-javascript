61d5561268b4b56320d7fb603dc00ace1263fa2d
Gabriel Isaac <stewarti3@yahoo.com>
2024-12-04 18:50:49 +0100

    fix again

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,15 @@
-#!/usr/bin/env node
+const readline = require('readline');
 
-console.log('Welcome to Holberton School, what is your name?');
-
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}`);
-  }
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing');
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}`);
+  rl.close();
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing\n');
 });
