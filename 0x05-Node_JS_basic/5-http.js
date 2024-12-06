e2c4dd98e2787da85944ec8467dc1994b7d884b2
Gabriel Isaac <stewarti3@yahoo.com>
2024-12-06 15:34:27 +0100

    refactor

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,48 +1,44 @@
 const http = require('http');
-const url = require('url');
 const countStudents = require('./3-read_file_async');
 
-const PORT = 1245;
-
-/**
- * Creates an HTTP server.
- * @type {http.Server}
- */
-const app = http.createServer((req, res) => {
-  const reqUrl = url.parse(req.url, true).pathname;
-
-  if (reqUrl === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+const app = http.createServer(async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  
+  if (req.url === '/') {
     res.end('Hello Holberton School!');
-  } else if (reqUrl === '/students') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-
-    const database = process.argv[2];
-    if (!database) {
-      res.end('This is the list of our students\nCannot load the database');
-      return;
+  } else if (req.url === '/students') {
+    try {
+      // Start with the initial text
+      res.write('This is the list of our students\n');
+      
+      // Capture console.log output
+      const originalConsoleLog = console.log;
+      let output = '';
+      console.log = (str) => {
+        output += str + '\n';
+      };
+      
+      // Process the database file
+      await countStudents(process.argv[2]);
+      
+      // Restore console.log
+      console.log = originalConsoleLog;
+      
+      // Send the captured output
+      res.end(output);
+    } catch (error) {
+      res.end(error.message);
     }
-
-    res.write('This is the list of our students\n');
-
-    countStudents(database)
-      .then(() => {
-        res.end();
-      })
-      .catch(() => {
-        res.end('Cannot load the database');
-      });
   } else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Not Found');
+    res.writeHead(404);
+    res.end('Not found');
   }
 });
 
+const PORT = 1245;
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port: ${PORT}`)
 });
 
 module.exports = app;
