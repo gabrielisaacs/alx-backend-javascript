867a1e905cb376dc0cf71dc350a8abfcd94365ea
Gabriel Isaac <stewarti3@yahoo.com>
2024-12-06 13:09:35 +0100

    complex http server

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const url = require('url');
+const countStudents = require('./3-read_file_async');
+
+const PORT = 1245;
+
+/**
+ * Creates an HTTP server.
+ * @type {http.Server}
+ */
+const app = http.createServer((req, res) => {
+  const reqUrl = url.parse(req.url, true).pathname;
+
+  if (reqUrl === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (reqUrl === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    const database = process.argv[2];
+    if (!database) {
+      res.end('This is the list of our students\nCannot load the database');
+      return;
+    }
+
+    res.write('This is the list of our students\n');
+
+    countStudents(database)
+      .then(() => {
+        res.end();
+      })
+      .catch(() => {
+        res.end('Cannot load the database');
+      });
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
