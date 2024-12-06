4ce5c3dd007b54d36377c9a73ca646da7ae74ea5
Gabriel Isaac <stewarti3@yahoo.com>
2024-12-06 16:56:11 +0100

    fix linter error

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const PORT = 1245;
 
@@ -11,7 +12,7 @@ app.listen(PORT, () => {
 });
 
 app.use((req, res) => {
-  res.status(404).send('<!DOCTYPE html>\n<html lang="en">\n<head>\n<meta charset="utf-8">\n<title>Error</title>\n</head>\n<body>\n<pre>Cannot GET ' + req.path + '</pre>\n</body>\n</html>');
+  res.status(404).send(`<!DOCTYPE html>\n<html lang="en">\n<head>\n<meta charset="utf-8">\n<title>Error</title>\n</head>\n<body>\n<pre>Cannot GET ${req.path}</pre>\n</body>\n</html>`);
 });
 
 module.exports = app;
